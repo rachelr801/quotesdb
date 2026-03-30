3307fab8a19957b7fd8e66a0c6725692372c4976
Rachel White <rrwhite80@gmail.com>
2026-03-30 16:34:53 -0500

    update header

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -2,15 +2,22 @@
 
 //  REQUIRED CORS HEADERS
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
 
-$method = $_SERVER['REQUEST_METHOD'];
+  header('Content-Type: application/json');
 
-if ($method === 'OPTIONS') {
+  $method = $_SERVER['REQUEST_METHOD'];
+
+
+
+  if ($method === 'OPTIONS') {
+
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+
     exit();
-}
+
+  }
 
 // ROUTING
 switch ($method) {
