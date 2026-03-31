aa9120ad42a9d7e5e3b87bb935b2e8b8a173fb61
Rachel White <rrwhite80@gmail.com>
2026-03-31 16:40:36 -0500

    cors

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -1,16 +1,23 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
 
-// Handle preflight
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  header('Content-Type: application/json');
+
+  $method = $_SERVER['REQUEST_METHOD'];
+
+
+
+  if ($method === 'OPTIONS') {
+
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-    exit();
-}
 
-$method = $_SERVER['REQUEST_METHOD'];
+    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+
+    exit();
+
+  }
+
 $id = isset($_GET['id']) ? $_GET['id'] : null;
 
 // Routing logic
