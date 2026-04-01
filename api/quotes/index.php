386eb06fc7a7a4959ef5494d8260bf2c6145e4ea
Rachel White <rrwhite80@gmail.com>
2026-03-31 19:07:10 -0500

    fix read

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -1,4 +1,5 @@
 <?php
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
@@ -16,20 +17,29 @@ if ($method === 'OPTIONS') {
 include_once('../config/Database.php');
 include_once('../models/Quote.php');
 
+// ✅ CREATE DB + OBJECT HERE
 $db = (new Database())->connect();
 $quote = new Quote($db);
 
+// ✅ THEN route
 switch($method) {
+
     case 'GET':
         require 'read.php';
         break;
+
     case 'POST':
         require 'create.php';
         break;
+
     case 'PUT':
         require 'update.php';
         break;
+
     case 'DELETE':
         require 'delete.php';
         break;
+
+    default:
+        echo json_encode(['message' => 'Invalid Request']);
 }
